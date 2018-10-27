2e1fdf3449cde03b17f495658382142c787444c5
Andrew Usher <andrewusher00@gmail.com>
2018-10-27 12:45:36 -0500

    Convert App component to function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import GlobalStyle from './GlobalStyle';
+import GlobalStyle from './GlobalStyle'
 import GeolocationWatch from './Geolocation/Watch'
 import GeolocationStatic from './Geolocation/Static'
 import Header from './Header'
@@ -12,33 +12,25 @@ import Orientation from './DeviceOrientation'
 import Resize from './WindowResize'
 import MouseMove from './MouseMove'
 
-
-
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <ThemeProvider
-          theme={{ primaryColor: '#26baee', primaryFont: 'Raleway' }}
-        >
-          <Fragment>
-            <GlobalStyle />
-            <Header />
-            <Switch>
-            <Route exact component={GeolocationWatch} path="/geolocation-watch" />
-              <Route exact component={Home} path="/" />
-              <Route exact component={GeolocationStatic} path="/geolocation-static" />
-              <Route exact component={Locale} path="/locale" />
-              <Route exact component={MouseMove} path="/mousemove" />
-              <Route exact component={Motion} path="/motion" />
-              <Route exact component={Orientation} path="/orientation" />
-              <Route exact component={Resize} path="/resize" />
-            </Switch>
-          </Fragment>
-        </ThemeProvider>
-      </BrowserRouter>
-    )
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <ThemeProvider theme={{ primaryColor: '#26baee', primaryFont: 'Raleway' }}>
+      <Fragment>
+        <GlobalStyle />
+        <Header />
+        <Switch>
+          <Route exact component={GeolocationWatch} path="/geolocation-watch" />
+          <Route exact component={Home} path="/" />
+          <Route exact component={GeolocationStatic} path="/geolocation-static" />
+          <Route exact component={Locale} path="/locale" />
+          <Route exact component={MouseMove} path="/mousemove" />
+          <Route exact component={Motion} path="/motion" />
+          <Route exact component={Orientation} path="/orientation" />
+          <Route exact component={Resize} path="/resize" />
+        </Switch>
+      </Fragment>
+    </ThemeProvider>
+  </BrowserRouter>
+)
 
 export default App
