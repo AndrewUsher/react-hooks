688e0b6b265905efea96d3f8ba17ec82345ca560
Andrew Usher <andrewusher00@gmail.com>
2018-10-27 12:43:43 -0500

    Move global style into seperate file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from './GlobalStyle';
 import GeolocationWatch from './Geolocation/Watch'
 import GeolocationStatic from './Geolocation/Static'
 import Header from './Header'
@@ -11,38 +12,7 @@ import Orientation from './DeviceOrientation'
 import Resize from './WindowResize'
 import MouseMove from './MouseMove'
 
-const GlobalStyle = createGlobalStyle`
-  *, *::before, *::after {
-    box-sizing: border-box;
-  }
 
-  body {
-    font-family: ${props => props.theme.primaryFont};
-    -webkit-font-smoothing: antialiased;
-	  -moz-osx-font-smoothing: grayscale;
-  }
-
-  section {
-    border: 1px solid #f5f5f5;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-    margin: 20px auto 0 auto;
-    max-width: 1200px;
-    padding: 10px 30px;
-    text-align: center;
-    transition: all 400ms;
-    width: 90%;
-
-    &:hover {
-      box-shadow: 0 14px 28px rgba(0,0,0,0.1), 0 10px 10px rgba(0,0,0,0.1);
-      transform: translateY(-3px);
-    }
-
-    ul {
-      margin: 0 auto;
-      text-align: left;
-    }
-  }
-`
 
 class App extends Component {
   render() {
