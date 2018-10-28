0266b5a32eea6309855abf21400ad6e8923ee6b3
Andrew Usher <andrewusher00@gmail.com>
2018-10-28 08:41:09 -0500

    Move Routes into seperate component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,8 @@ import React, { Fragment } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from './GlobalStyle'
-import GeolocationWatch from './Geolocation/Watch'
-import GeolocationStatic from './Geolocation/Static'
 import Header from './Header'
-import Home from './Home'
-import Locale from './Locale'
-import Motion from './DeviceMotion'
-import Orientation from './DeviceOrientation'
-import Resize from './WindowResize'
-import MouseMove from './MouseMove'
-import PreviousState from './PreviousState'
-import Fetch from './Fetch'
+import Routes from './Routes'
 
 const App = () => (
   <BrowserRouter>
@@ -20,18 +11,7 @@ const App = () => (
       <Fragment>
         <GlobalStyle />
         <Header />
-        <Switch>
-          <Route exact component={GeolocationWatch} path="/geolocation-watch" />
-          <Route exact component={Fetch} path="/fetch" />
-          <Route exact component={PreviousState} path="/previous-state" />
-          <Route exact component={Home} path="/" />
-          <Route exact component={GeolocationStatic} path="/geolocation-static" />
-          <Route exact component={Locale} path="/locale" />
-          <Route exact component={MouseMove} path="/mousemove" />
-          <Route exact component={Motion} path="/motion" />
-          <Route exact component={Orientation} path="/orientation" />
-          <Route exact component={Resize} path="/resize" />
-        </Switch>
+        <Routes />
       </Fragment>
     </ThemeProvider>
   </BrowserRouter>
